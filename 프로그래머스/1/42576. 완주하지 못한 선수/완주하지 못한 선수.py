09db3b7bd7984f420e4fc0d970dc10e6535b7a24
choisiwon <qm0430@naver.com>
2026-09-14 20:30:04 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 40.37 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,4 +1,11 @@
-from collections import Counter
 def solution(participant, completion):
-    answer = list((Counter(participant) - Counter(completion)))[0]
+    answer = ''
+    temp = 0
+    d = {}
+    for part in participant:
+        d[hash(part)] = part
+        temp += int(hash(part))
+    for com in completion:
+        temp -= int(hash(com))
+    answer = d[temp]
     return answer
